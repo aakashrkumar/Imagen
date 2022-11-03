e344b4d7d62d24ccfed238f097cc9324077ba9a9
TheRealAakash <aakashinthesky@gmail.com>
2022-11-03 14:04:47 -0700

    Update

--- imagen.py
+++ imagen.py
@@ -8,22 +8,22 @@ import jax.numpy as jnp
 
 class ResNetBlock(nn.Module):
     """ResNet block with a projection shortcut and batch normalization."""
-    num_layers: int = 1
-    num_channels: int = 1
-    strides: Tuple[int, int] = (2, 2)
+    num_layers: int
+    num_channels: int
+    strides: Tuple[int, int]
     dtype: jnp.dtype = jnp.float32
     training: bool = True
 
     @nn.compact
     def __call__(self, x):
-        for i in range(self.num_layers):
+        for _ in range(self.num_layers):
             residual = x
             x = nn.GroupNorm(dtype=self.dtype)(x)
             x = nn.swish(x)
-            x = nn.Conv(features=self.num_channels, kernel_size=(3, 3), strides=self.strides, dtype=self.dtype)(x)
+            x = nn.Conv(self.num_channels, kernel_size=(3, 3), strides=self.strides, dtype=self.dtype, padding="same")(x)
             x = nn.GroupNorm(dtype=self.dtype)(x)
             x = nn.swish(x)
-            x = nn.Conv(features=self.num_channels, kernel_size=(3, 3), dtype=self.dtype)(x)
+            x = nn.Conv(self.num_channels, kernel_size=(3, 3), strides=self.strides, dtype=self.dtype, padding="same")(x)
             residual = nn.Conv(features=self.num_channels, kernel_size=(1, 1), strides=self.strides, dtype=self.dtype)(residual)
             x = x + residual
         return nn.relu(x)
@@ -37,21 +37,37 @@ class CombineEmbs(nn.Module):
 
 class UnetDBlock(nn.Module):
     """UnetD block with a projection shortcut and batch normalization."""
-    num_channels: int = 1
-    strides: Tuple[int, int] = (2, 2)
+    num_channels: int
+    strides: Tuple[int, int]
     dtype: jnp.dtype = jnp.float32
 
     @nn.compact
     def __call__(self, x):
-        x = nn.Conv(features=self.num_channels, kernel_size=(3, 3), strides=self.strides, dtype=self.dtype)(x)
+        x = nn.Conv(features=self.num_channels, kernel_size=(3, 3), strides=self.strides, dtype=self.dtype, padding=1)(x)
         x = ResNetBlock(num_layers=3, num_channels=self.num_channels, strides=self.strides, dtype=self.dtype)(x)
-        x = nn.SelfAttention(num_heads=8,out_features=self.channels,)(x)
+        #x = nn.SelfAttention(num_heads=8, qkv_features=2 * self.num_channels, out_features=self.num_channels)(x)
         return x
 
-# 3 *  64 x 64 -> 3 * 32 x 32
-# 3 *  32 x 32 -> 3 * 16 x 16
-# 3 *  16 x 16 -> 3 * 8 x 8
-module = UnetDBlock(num_channels=3)
-params = module.init(jax.random.PRNGKey(0), jnp.ones((1, 3, 64, 64)))
-x = module.apply(params, jnp.ones((1, 3, 64, 64)))
-print(x.shape)
+class EfficentUNet(nn.Module):
+    strides: Tuple[int, int] = (1, 1)
+    dtype: jnp.dtype = jnp.float32
+    
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Conv(features=128, kernel_size=(3, 3), strides=self.strides, dtype=self.dtype, padding="same")(x)
+        x = UnetDBlock(num_channels=128, strides=self.strides, dtype=self.dtype)(x)
+        x = UnetDBlock(num_channels=256, strides=self.strides, dtype=self.dtype)(x)
+        x = UnetDBlock(num_channels=512, strides=self.strides, dtype=self.dtype)(x)
+        return x
+
+def test():
+    # 3 *  64 x 64 -> 3 * 32 x 32
+    # 3 *  32 x 32 -> 3 * 16 x 16
+    # 3 *  16 x 16 -> 3 * 8 x 8
+    module = EfficentUNet()
+    images = jnp.ones((1, 256, 256, 3))
+    params = module.init(jax.random.PRNGKey(0), images)
+    x = module.apply(params, images)
+    print(x.shape)
+    
+test()
