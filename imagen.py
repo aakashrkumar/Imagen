17447294ca602f0d80c101b4ee8ea025324137a0
TheRealAakash <aakashinthesky@gmail.com>
2022-11-04 10:47:21 -0700

    Update

--- imagen.py
+++ imagen.py
@@ -63,17 +63,62 @@ class UnetDBlock(nn.Module):
         #x = nn.SelfAttention(num_heads=8, qkv_features=2 * self.num_channels, out_features=self.num_channels)(x)
         return x
 
-class EfficentUNet(nn.Module):
-    strides: Tuple[int, int] = (1, 1)
+class UnetUBlock(nn.Module):
+    """UnetU block with a projection shortcut and batch normalization."""
+    num_channels: int
+    strides: Tuple[int, int]
     dtype: jnp.dtype = jnp.float32
-    
+    num_resnet_blocks: int = 3
+    text_cross_attention: bool = False
+    num_attention_heads: int = 0
+
     @nn.compact
     def __call__(self, x):
-        x = nn.Conv(features=128, kernel_size=(3, 3), strides=self.strides, dtype=self.dtype, padding="same")(x)
-        x = UnetDBlock(num_channels=128, strides=self.strides, dtype=self.dtype)(x)
-        x = UnetDBlock(num_channels=256, strides=self.strides, dtype=self.dtype)(x)
-        x = UnetDBlock(num_channels=512, strides=self.strides, dtype=self.dtype)(x)
+        # combine embs
+        x = ResNetBlock(num_layers=self.num_resnet_blocks,
+                        num_channels=self.num_channels, strides=self.strides, dtype=self.dtype)(x)
+        if self.num_attention_heads > 0:
+            x = nn.SelfAttention(num_heads=self.num_attention_heads, qkv_features=2 *
+                                  self.num_channels, out_features=self.num_channels, shard_axes={"kernel": ("heads", "hidden")})(x)
+        x = jax.image.resize(
+            x,
+            shape=(x.shape[0], x.shape[1] * 2, x. shape[2] * 2, x.shape[3]),
+            method="nearest",
+        )
+        x = nn.Conv(features=self.num_channels, kernel_size=(
+            3, 3), dtype=self.dtype, padding=1)(x)
         return x
+class EfficentUNet(nn.Module):
+    strides: Tuple[int, int] = (2, 2)
+    dtype: jnp.dtype = jnp.float32
+
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Conv(features=128, kernel_size=(3, 3),
+                     strides=self.strides, dtype=self.dtype, padding="same")(x)
+        uNet256D = UnetDBlock(num_channels=128, strides=self.strides,
+                              num_resnet_blocks=2, dtype=self.dtype)(x)
+        # return uNet256D
+        uNet64D = UnetDBlock(num_channels=256, strides=self.strides,
+                             num_resnet_blocks=4, dtype=self.dtype)(uNet256D)
+        uNet32D = UnetDBlock(num_channels=512, strides=self.strides,
+                             num_resnet_blocks=8, dtype=self.dtype)(uNet64D)
+        uNet16D = UnetDBlock(num_channels=1024, strides=self.strides,
+                             num_resnet_blocks=8, num_attention_heads=8, dtype=self.dtype)(uNet32D)
+
+        uNet16U = UnetUBlock(num_channels=1024, strides=self.strides,
+                             num_resnet_blocks=8, num_attention_heads=8, dtype=self.dtype)(uNet16D)
+        uNet32U = UnetUBlock(num_channels=512, strides=self.strides,
+                             num_resnet_blocks=8, dtype=self.dtype)(jnp.concatenate([uNet16U, uNet32D], axis=-1))
+        uNet64U = UnetUBlock(num_channels=256, strides=self.strides,
+                             num_resnet_blocks=4, dtype=self.dtype)(jnp.concatenate([uNet32U, uNet64D], axis=-1))
+        uNet256U = UnetUBlock(num_channels=128, strides=self.strides,
+                              num_resnet_blocks=2, dtype=self.dtype)(jnp.concatenate([uNet64U, uNet256D], axis=-1))
+
+        x = nn.Dense(features=256 * 256 * 3, dtype=self.dtype)(uNet256U)
+        return uNet256U
+
+
 
 def test():
     # 3 *  64 x 64 -> 3 * 32 x 32
