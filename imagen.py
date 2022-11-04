ab403e7fecaef257b3e03b0d99f4ae83d8443d8f
TheRealAakash <aakashinthesky@gmail.com>
2022-11-04 06:24:10 -0700

    Update

--- imagen.py
+++ imagen.py
@@ -156,10 +156,10 @@ def test():
     # 3 *  16 x 16 -> 3 * 8 x 8
     
     module = EfficentUNet()
-    images = jnp.ones((16, 256, 256, 3))
+    images = jnp.ones((1, 256, 256, 3))
     with maps.Mesh(mesh.devices, mesh.axis_names), nn_partitioning.axis_rules(TPU_RULES):
-        params = jax.eval_shape(module.init, jax.random.PRNGKey(0), images)
-        pjitForward = pjit.pjit(module.apply, in_axis_resources=P(None, None), out_axis_resources=P("X", None, "Y"))
+        params = pjit.pjit(module.init, in_axis_resources=(None, P("X", None)), out_axis_resources=P("X", None, "Y"))(jax.random.PRNGKey(0), images)
+        pjitForward = pjit.pjit(module.apply, in_axis_resources=P("X", None), out_axis_resources=P("X", None, "Y"))
         for i in range(100):
             x = pjitForward(params, images)
             print(x.shape)
