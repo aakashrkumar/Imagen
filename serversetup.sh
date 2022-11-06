ec04a671b8e42c89790d1d71e7aed835da0d4bca
aakash <aakashinthesky@gmail.com>
2022-11-05 22:47:57 -0700

    Update

--- serversetup.sh
+++ serversetup.sh
@@ -3,3 +3,4 @@ pip install jax
 pip install -U "jax[tpu]>=0.2.16" jaxlib -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 sudo pip install -U "jax[tpu]>=0.2.16" jaxlib -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install -U flax
+pip install wandb
