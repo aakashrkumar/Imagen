09907011dca1d6fe00134ebe304a66443a5d1939
aakash <aakashinthesky@gmail.com>
2022-11-19 13:07:09 -0800

    Update

--- imagen.py
+++ imagen.py
@@ -17,42 +17,62 @@ class ResNetBlock(nn.Module):
 
     @nn.compact
     def __call__(self, x):
+        # Iterate over the number of layers.
         for _ in range(self.num_layers):
+            # Save the input for the residual connection.
             residual = x
+
+            # Normalization, swish, and convolution.
             x = nn.GroupNorm(dtype=self.dtype)(x)
             x = nn.swish(x)
             x = nn.Conv(self.num_channels, kernel_size=(3, 3),
                         dtype=self.dtype, padding="same")(x)
+
+            # Normalization, swish, and convolution.
             x = nn.GroupNorm(dtype=self.dtype)(x)
             x = nn.swish(x)
             x = nn.Conv(self.num_channels, kernel_size=(3, 3),
                         dtype=self.dtype, padding="same")(x)
+
+            # Projection shortcut.
             residual = nn.Conv(features=self.num_channels,
                                kernel_size=(1, 1), dtype=self.dtype)(residual)
+
+            # Add the residual connection.
             x = x + residual
         return x
 
 
 class CombineEmbs(nn.Module):
+    """Combine positional encoding with text/image encoding."""
+
     d: int = 32  # should be the dimensions of x
     n: int = 10000  # user defined scalor
 
     @nn.compact
     def __call__(self, x, t):
         # timestep encoding
+        # 1. Create a position encoding matrix with shape (1, d) where d = input dimension.
+        #    The matrix will be initialized with zeros.
         d = x.shape[-1]
         pe = jnp.zeros((1, d))
+        # 2. Create a position encoding vector with shape (1, 1) where the value is the timestep.
+        #    The vector will be initialized with zeros.
         position = jnp.array([t]).reshape(-1, 1)
+        # 3. Calculate a denominator term for the sine and cosine functions.
         div_term = jnp.power(self.n, jnp.arange(0, d, 2) / d)
+        # 4. Calculate the sine and cosine terms for the position encoding vector.
+        #    Add the sine and cosine terms to the position encoding matrix.
         pe = pe.at[:, 0::2].set(jnp.sin(position * div_term))
         pe = pe.at[:, 1::2].set(jnp.cos(position * div_term))
+        # 5. Repeat the position encoding matrix for each batch and spatial dimension.
         pe = pe[jnp.newaxis, jnp.newaxis, :]
-        pe = jnp.repeat(pe, x.shape[1], axis=1)
-        pe = jnp.repeat(pe, x.shape[2], axis=2)
+        pe = jnp.repeat(pe, x.shape[1], axis=1) # repeat for each spatial dimension
+        pe = jnp.repeat(pe, x.shape[2], axis=2) # repeat for each batch
+        # 6. Add the position encoding to the input.
         x = x + pe  
         # TODO add text/image encoding to x
         return x
-
 
 class UnetDBlock(nn.Module):
     """UnetD block with a projection shortcut and batch normalization."""
