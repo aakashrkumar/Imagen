c3771511f79bb6221486bd02e382ca27dc2092a0
aakash <aakashinthesky@gmail.com>
2022-11-20 16:15:50 -0800

    Update

--- train.py
+++ train.py
@@ -6,9 +6,42 @@ import jax.numpy as jnp
 from imagen_main import Imagen
 import wandb
 import numpy as np
+from datasets import load_dataset
+
+from functools import partial
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+import io
+import urllib
+
+import PIL.Image
+
+from datasets import load_dataset
+from datasets.utils.file_utils import get_datasets_user_agent
+
 
 wandb.init(project="imagen")
 
+USER_AGENT = get_datasets_user_agent()
+
+
+def fetch_single_image(image_url, timeout=None, retries=0):
+    for _ in range(retries + 1):
+        try:
+            request = urllib.request.Request(
+                image_url,
+                data=None,
+                headers={"user-agent": USER_AGENT},
+            )
+            with urllib.request.urlopen(request, timeout=timeout) as req:
+                image = PIL.Image.open(io.BytesIO(req.read())).resize((config.image_size, config.image_size))
+                # convert to array H, W, C
+                image = np.array(image)[..., :3] / 255.0
+                
+            break
+        except Exception:
+            image = None
+    return image
 
 class config:
     batch_size = 8
@@ -18,9 +51,19 @@ class config:
     save_every = 1000
     eval_every = 1000
     steps = 100_000
+    
 
+def fetch_images(batch, num_threads, timeout=None, retries=0):
+    fetch_single_image_with_args = partial(fetch_single_image, timeout=timeout, retries=retries)
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        batch["image"] = list(executor.map(fetch_single_image_with_args, batch["image_url"]))
+    return batch
 
 def train(imagen: Imagen, steps):
+    dataset = load_dataset("red_caps", split="train")
+    dataset = dataset.map(fetch_images, batched=True, batch_size=16, fn_kwargs={"num_threads": 20})
+
+
     for step in tqdm(range(1, steps + 1)):
         images = jax.random.normal(
             imagen.get_key(), (config.batch_size, config.image_size, config.image_size, 3))
@@ -35,7 +78,7 @@ def train(imagen: Imagen, steps):
             # log as 16 gifs
             gifs = []
             for i in range(16):
-                gifs.append(wandb.Video(np.asarray(imgs[i]), fps=60, format="gif"))
+                gifs.append(wandb.Video(np.array(imgs[i] * 255, dtype=np.uint8), fps=60, format="gif"))
             wandb.log({"samples": gifs})
         wandb.log(metrics)
 
