ebd0502b061e8fea091306d55b6bfaad335436db
AmanM <QuackingBob>
2022-11-23 12:37:17 -0800

    update

---
+++ imagen_main-Aman-asus.py
@@ -0,0 +1,153 @@
+from functools import partial
+from modeling_imagen import EfficentUNet
+from typing import Any, Dict, Tuple
+import jax
+import flax
+from flax import linen as nn
+import jax.numpy as jnp
+
+from tqdm import tqdm
+
+import optax
+
+from sampler import GaussianDiffusionContinuousTimes, extract
+from einops import rearrange, repeat, reduce, pack, unpack
+from flax.training import train_state
+
+from jax import tree_util
+from flax import struct
+
+
+class ImagenState(struct.PyTreeNode):
+    train_state: train_state.TrainState
+    sampler: GaussianDiffusionContinuousTimes
+    
+class GeneratorState(struct.PyTreeNode):
+    imagen_state: ImagenState
+    image: jnp.ndarray
+    text: jnp.ndarray
+    rng: jax.random.PRNGKey
+    
+    
+
+def j_sample(state, sampler, x, texts, t, t_index, rng):
+    betas_t = extract(sampler.betas, t, x.shape)
+    sqrt_one_minus_alphas_cumprod_t = extract(
+        sampler.sqrt_one_minus_alphas_cumprod, t, x.shape)
+    sqrt_recip_alphas_t = extract(
+        sampler.sqrt_recip_alphas, t, x.shape)
+    model_mean = sqrt_recip_alphas_t * \
+        (x - betas_t * state.apply_fn({"params": state.params}, x, texts, t) /
+            sqrt_one_minus_alphas_cumprod_t)
+   # s = jnp.percentile(
+    #    jnp.abs(model_mean), 0.95,
+      #  axis=(1, *tuple(range(1, model_mean.ndim)))
+     #   )
+    # s = jnp.max(s, 1.0)
+    
+    model_mean = jnp.clip(model_mean, -1., 1.)
+    
+    return model_mean
+
+def p_sample(t_index, state):
+    t_index = 999-t_index
+    t = jnp.ones(1, dtype=jnp.int16) * t_index
+    rng, key = jax.random.split(state.rng)
+    model_mean = j_sample(state.imagen_state.train_state, state.imagen_state.sampler, state.image, state.text, t, t_index, key)
+    rng, key = jax.random.split(rng)
+    posterior_variance_t = extract(
+    state.imagen_state.sampler.posterior_variance, t, state.image.shape)
+    noise = jax.random.normal(key, state.image.shape)  # TODO: use proper key
+
+    x = jax.lax.cond(t_index > 0, lambda x: model_mean + noise * jnp.sqrt(posterior_variance_t), lambda x: model_mean, None)
+   #if t_index == 0:
+   #     x = model_mean
+   # else:
+    #    x = model_mean + noise * jnp.sqrt(posterior_variance_t)
+    return GeneratorState.replace(state, image=x, rng=rng)
+@jax.jit
+def p_sample_loop(state, img, texts, rng):
+    # img is x0
+    batch_size = img.shape[0]
+    rng, key = jax.random.split(rng)
+    # imgs = []
+    generator_state = GeneratorState(imagen_state=state, image=img, text=texts, rng=rng)
+    generator_state = jax.lax.fori_loop(0, 1000, p_sample, generator_state)
+    img = generator_state.image
+    #for i in reversed(range(sampler.num_timesteps)):
+     #  rng, key = jax.random.split(rng)
+     #  img = p_sample(state, sampler, img, texts, jnp.ones(batch_size, dtype=jnp.int16) * i, i, key)
+       # imgs.append(img)
+    # frames, batch, height, width, channels
+    # reshape batch, frames, height, width, channels
+    return img
+
+
+def sample(state, noise, texts, rng):
+    return p_sample_loop(state, noise, texts, rng)
+
+@jax.jit
+def train_step(state, sampler, x, texts, timestep, rng):
+    noise = jax.random.normal(rng, x.shape)
+    x_noise = sampler.q_sample(x, timestep, noise)
+    def loss_fn(params):
+        predicted = state.apply_fn({"params": params}, x_noise, texts, timestep)
+        loss = jnp.mean((noise - predicted) ** 2)
+        return loss, predicted
+    gradient_fn = jax.value_and_grad(loss_fn, has_aux=True)
+    (loss, logits), grads = gradient_fn(state.params)
+    train_state = state.train_state.apply_gradients(grads=grads)
+    state = state.replace(train_state=train_state)
+    return state, compute_metrics(loss, logits)
+
+
+
+class Imagen:
+    def create(self, img_size: int = 64, batch_size: int = 16, num_timesteps: int = 1000, loss_type: str = "l2"):
+        self.random_state = jax.random.PRNGKey(0)        
+        self.lowres_scheduler = GaussianDiffusionContinuousTimes.create(
+            noise_schedule="cosine", num_timesteps=1000
+        )
+        self.unet = EfficentUNet()
+        self.params = self.unet.init(self.get_key(), jnp.ones((batch_size, img_size, img_size, 3)), None, jnp.ones(batch_size, dtype=jnp.int16))
+        
+        self.opt = optax.adafactor(1e-4)
+        self.train_state = train_state.TrainState.create(
+            apply_fn=self.unet.apply,
+            tx=self.opt,
+            params=self.params['params']
+        )
+        self.state = ImagenState(train_state=self.train_state, sampler=self.lowres_scheduler)
+        
+        self.image_size = img_size
+
+    def get_key(self):
+        self.random_state, key = jax.random.split(self.random_state)
+        return key
+    
+    def sample(self, texts, batch_size):
+        noise = jax.random.normal(self.get_key(), (batch_size, self.image_size, self.image_size, 3))
+        return sample(self.state, noise, texts, self.get_key())
+    
+    def train_step(self, image_batch, texts_batchs, timestep):
+        train_state, metrics = train_step(self.state, image_batch, texts_batchs, timestep, self.get_key())
+        return metrics
+
+def compute_metrics(loss, logits):
+    return {"loss": loss}
+
+def test():
+    import cv2
+    import numpy as np
+    imagen = Imagen()
+    #train_step(imagen.state, imagen.lowres_scheduler, jnp.ones((1, 64, 64, 3)), None, jnp.ones(1, dtype=jnp.int16), imagen.get_key())
+    print("Training done")
+    for i in tqdm(range(1000)):
+        images = imagen.sample(None, 1)
+        print(images.shape)
+        images = np.asarray(images * 127.5 + 127.5, dtype=np.uint8)
+        # cv2.imshow("image", images[i])
+        #cv2.waitKey(0)
+if __name__ == "__main__":
+    test()
+    
--- testt5.py
+++ testt5.py
@@ -28,3 +28,28 @@ print(outputs["attentions"][-1].shape)
 
 
 # def pool_sequences(input_id):
+
+def get_tokenizer_and_model():
+    name = "t5-large"
+    tokenizer = T5Tokenizer.from_pretrained(name)
+    model = FlaxT5ForConditionalGeneration.from_pretrained(name)
+
+def encode_text(text, tokenizer, model):
+    max_sequence_length = 512
+    max_target_length = 128
+    encoding = tokenizer(
+        text,
+        padding="longest", 
+        max_length=max_sequence_length, 
+        truncation=True, 
+        return_tensors="np")
+    
+    input_ids, attention_mask = encoding.input_ids, encoding.attention_mask
+
+    outputs = model.encode(
+        input_ids=input_ids, 
+        attention_mask=attention_mask,
+        return_dict=True,
+        output_attentions=False)
+
+    return outputs, attention_mask
