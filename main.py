7a1cd2fdd7e24f4acf6371bf6922bf3d9df2f440
aakash <aakashinthesky@gmail.com>
2022-11-24 19:09:41 -0800

    Update

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ ray.init(address="auto")
 
 
 for i in range(10):
-    ray_tpu.create_tpu(f"ray-tpu-{i}", "us-central1-f", "v2-8", True)
-    
+    # ray_tpu.create_tpu(f"ray-tpu-{i}", "us-central1-f", "v2-8", True)
+    ray_tpu.wait_til(f"ray-tpu-{i}", "us-central1-f", {"state": "READY"})
+    ray_tpu.start_ray(ray_tpu.get_connection(f"ray-tpu-{i}", "us-central1-f")[0], "globaltpu2.aakashserver.org")
