82c4f283a3fb52687f90aa87f271f8f95bb9ff4a
aakash <aakashinthesky@gmail.com>
2022-11-24 20:18:05 -0800

    Update

--- scripts/serversetup.sh
+++ scripts/serversetup.sh
@@ -3,4 +3,4 @@ bash Miniconda3-latest-Linux-x86_64.sh -b -p /home/aakash/miniconda3
 source ~/miniconda3/bin/activate
 pip install -U "jax[tpu]>=0.2.16" jaxlib -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 sudo pip install -U "jax[tpu]>=0.2.16" jaxlib -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-pip install -r scripts/requirements.txt
+pip install -r Imagen/scripts/requirements.txt
