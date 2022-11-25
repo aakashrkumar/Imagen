507652e0defc8b952e5692cbbc70c882a939b35d
aakash <aakashinthesky@gmail.com>
2022-11-25 12:12:37 -0800

    Update

--- TPUManager.py
+++ TPUManager.py
@@ -14,6 +14,10 @@ class TPUManager:
             ray_tpu.create_tpu(tpu, "us-central1-f", "v2-8", True)
         for tpu in self.tpus:
             ray_tpu.wait_til(tpu, 'us-central1-f', {"state": "READY"})
+        threads = []
         for tpu in self.tpus:
-            ray_tpu.start_ray(tpu, "us-central1-f", self.address)
+            threads.append(threading.Thread(target=ray_tpu.start_ray, args=(tpu, "us-central1-f", self.address)))
+            threads[-1].start()
+        for thread in threads:
+            thread.join()
         return True
