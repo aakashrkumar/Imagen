9d5f28c5afcdc5eefe5ee3b869df275e4032b41c
aakash <aakashinthesky@gmail.com>
2022-11-24 19:06:50 -0800

    Update

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ ray.init(address="auto")
 
 
 for i in range(10):
-    ray_tpu.create_tpu(f"ray-tpu-{i}", "us-central1-f")
+    ray_tpu.create_tpu(f"ray-tpu-{i}", "us-central1-f", "v2-8", True)
     
