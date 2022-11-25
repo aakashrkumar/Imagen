dc084e49b13e1b6ce53078afdd03e3d49b981c24
aakash <aakashinthesky@gmail.com>
2022-11-25 15:47:02 -0800

    Update

--- dataCollector.py
+++ dataCollector.py
@@ -149,9 +149,11 @@ class Processor:
     
     def start_encoding(self):
         while True:
-            images, texts = ray.get(self.shared_storage.get_batch_unencoded.remote(32))
-            texts_encoded, attention_masks = ray.get(self.encoder.encode.remote(texts))
-            self.shared_storage.add_data.remote(images, texts, texts_encoded, attention_masks)
+            out = ray.get(self.shared_storage.get_batch_unencoded.remote(32))
+            if out:
+                images, texts = out
+                texts_encoded, attention_masks = ray.get(self.encoder.encode.remote(texts))
+                self.shared_storage.add_data.remote(images, texts, texts_encoded, attention_masks)
 
     
 @ray.remote(num_cpus=2, resources={"host": 1})
