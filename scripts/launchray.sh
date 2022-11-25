6fba5d4d00af131ce7aeac9af7c870005b2f15c8
aakash <aakashinthesky@gmail.com>
2022-11-24 18:55:42 -0800

    Update

--- scripts/launchray.sh
+++ scripts/launchray.sh
@@ -1,2 +1,2 @@
 ray  stop -f || true
-TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=34359738368 ray start --address=globaltpu2.aakashserver.org --resources="{\"tpu\": 1}"
+TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=34359738368 ray start --head --port=6379 --address=globaltpu2.aakashserver.org --resources="{\"tpu\": 1}"
