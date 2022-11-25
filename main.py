ec3fceb953a6b29615cc12f4e5b2fe3d6f7664b7
aakash <aakashinthesky@gmail.com>
2022-11-24 19:04:38 -0800

    Update

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 import ray_tpu
+import ray
+
+ray.init(address="auto")
+
 
--- scripts/launchray.sh
+++ scripts/launchray.sh
@@ -1,2 +1,2 @@
 ray  stop -f || true
-TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=34359738368 ray start --head --port=6379 --node-ip-address=globaltpu2.aakashserver.org --resources="{\"tpu\": 1}"
+TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=34359738368 ray start --head --port=6379 --node-ip-address=globaltpu2.aakashserver.org --resources="{\"tpu\": 1}" --dashboard-host=0.0.0.0
