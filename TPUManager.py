9dd94cf4436813b3ae37caa350189c9282fbc51b
aakash <aakashinthesky@gmail.com>
2022-11-25 11:59:55 -0800

    Update

--- TPUManager.py
+++ TPUManager.py
@@ -7,7 +7,6 @@ class TPUManager:
     def __init__(self, num_tpus, address):
         self.num_tpus = num_tpus
         self.tpus = ["ray-tpu-" + str(i) for i in range(1, num_tpus + 1)]
-        self.conns = []
         self.address = address
 
     def setup(self):
@@ -16,7 +15,7 @@ class TPUManager:
         for tpu in self.tpus:
             ray_tpu.wait_til(tpu, 'us-central1-f', {"state": "READY"})
         for tpu in self.tpus:
-            self.conns += ray_tpu.get_connection(tpu, "us-central1-f")
+            conns += ray_tpu.get_connection(tpu, "us-central1-f")
 
         for conn in self.conns:
             ray_tpu.start_ray(conn, self.address)
