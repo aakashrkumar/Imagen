785ba78ee607d54014395ac3ff108f55c6cf95a4
aakash <aakashinthesky@gmail.com>
2022-11-27 12:17:36 -0800

    Update

--- testSpeed.py
+++ testSpeed.py
@@ -9,8 +9,9 @@ import TPUManager
 ray.init()
 
 tpuManager = TPUManager.TPUManager(6, "globaltpu2.aakashserver.org:6379")
-tpuManager.setup()
+# tpuManager.setup()
 collector = dataCollector.DataManager.remote(num_workers=90, batch_size=64)
+collector.start.remote()
 # T5Encoder = dataCollector.T5Encoder.remote()
 pb = tqdm.tqdm(total=1000000)
 while True:
