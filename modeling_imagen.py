fe14fe00d61b65c8dc7bfa69738bf8d1bbca52f9
aakash <aakashinthesky@gmail.com>
2022-12-01 21:53:05 -0800

    Update

--- modeling_imagen.py
+++ modeling_imagen.py
@@ -279,6 +279,7 @@ class CrossEmbedLayer(nn.Module):
         dim_scales = [int(self.dim_out / (2 ** i))
                       for i in range(1, len(self.kernel_sizes))]
         dim_scales = [*dim_scales, self.dim_out - sum(dim_scales)]
+        # TODO: most implementations have axis=1, but we're using -1
         return jnp.concatenate([nn.Conv(features=dim_scale, kernel_size=(kernel_size, kernel_size), strides=self.stride, padding=(kernel_size - self.stride) // 2)(x) for dim_scale, kernel_size in zip(dim_scales, self.kernel_sizes)], axis=-1)
 
 class TextConditioning(nn.Module):
