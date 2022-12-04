cf9f78148a99851103ae59e57c8d236226275cc1
aakash <aakashinthesky@gmail.com>
2022-12-03 22:23:40 -0800

    Update

--- layers.py
+++ layers.py
@@ -22,6 +22,9 @@ class EinopsToAndFrom(nn.Module):
     def __call__(self, x, **kwargs):
         shape = x.shape
         reconstitute_kwargs = dict(tuple(zip(self.from_einops.split(' '), shape)))
+        print(x)
+        print(self.from_einops)
+        print(self.to_einops)
         x = rearrange(f"{self.from_einops} -> {self.to_einops}", x)
         x = self.fn(x, **kwargs)
         x = rearrange(f"{self.to_einops} -> {self.from_einops}", x, **reconstitute_kwargs)
