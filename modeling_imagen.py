e3647553033bed808adeef90c6ccfd5631f3ffce
aakash <aakashinthesky@gmail.com>
2022-12-03 23:18:39 -0800

    Update

--- modeling_imagen.py
+++ modeling_imagen.py
@@ -119,7 +119,7 @@ class EfficentUNet(nn.Module):
         
         x = ResnetBlock(dim=self.dim * self.dim_mults[-1], cond_dim=cond_dim, dtype=self.dtype)(x, t, c)
         x = EinopsToAndFrom(Attention(self.dim * self.dim_mults[-1]), 'b h w c', 'b (h w) c')(x)
-        x = ResnetBlock(dim=self.dim * self.dim_mults[-1], dtype=self.dtype)(x, t, c)
+        x = ResnetBlock(dim=self.dim * self.dim_mults[-1], cond_dim=cond_dim, dtype=self.dtype)(x, t, c)
         
         # Upsample
         add_skip_connection = lambda x: jnp.concatenate([x, hiddens.pop()], axis=-1)
