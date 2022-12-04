789d7bdaefd014b8ad0b5acff1c8f78d94dbe0c8
aakash <aakashinthesky@gmail.com>
2022-12-03 19:35:40 -0800

    Update

--- Trainer.py
+++ Trainer.py
@@ -37,6 +37,7 @@ def get_datasets():
         img, (64, 64)) for img in train_ds['image']], axis=0)
     train_ds['image'] = np.stack(
         [cv2.cvtColor(img, cv2.COLOR_GRAY2RGB) for img in train_ds['image']], axis=0)
+    print(train_ds['image'][0])
     train_ds["image"] =  np.array(train_ds["image"], dtype=np.float32)
     return train_ds, test_ds
 
