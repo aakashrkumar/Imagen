8924d2af82a61088dbacb542d1ae08ffd2109a39
TheRealAakash <aakashinthesky@gmail.com>
2022-12-07 13:15:23 -0800

    Update

--- modeling_imagen.py
+++ modeling_imagen.py
@@ -38,6 +38,7 @@ class EfficentUNet(nn.Module):
         time_tokens = nnp.Dense(cond_dim * self.num_time_tokens, dtype=self.dtype, shard_axes={"kernel": ("embed_kernel", None)})(t)
         time_tokens = rearrange(time_tokens, 'b (r d) -> b r d', r=self.num_time_tokens)
         time_tokens = with_sharding_constraint(time_tokens, ("batch", "tokens", "embed"))
+        print("time_tokens", time_tokens.shape)
         
         t, c = TextConditioning(cond_dim=cond_dim, time_cond_dim=time_conditioning_dim, max_token_length=self.max_token_len, cond_drop_prob=condition_drop_prob)(texts, attention_masks, t, time_tokens, rng)
         # TODO: add lowres conditioning
