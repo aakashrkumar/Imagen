7397cff50132179f1ced51da05e2f9b55ea206de
aakash <aakashinthesky@gmail.com>
2022-12-07 07:42:13 -0800

    Update

--- imagen_main.py
+++ imagen_main.py
@@ -175,9 +175,11 @@ def compute_metrics(loss, logits):
 def test():
     imagen = Imagen()
     batch_size = 16
+    pb = tqdm(range(100000))
     while True:
         imagen.train_step(jnp.ones((batch_size, 64, 64, 3)), jnp.ones((batch_size,)) * 1, jnp.ones((batch_size, 256, 512)), jnp.ones((batch_size, 256)))
-        print("done")
+        pb.update(1)
+#        print("done")
 
 if __name__ == "__main__":
     test()
