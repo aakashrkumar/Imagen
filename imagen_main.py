27e8e2abbf02703203b160b40979884811d8d97f
TheRealAakash <aakashinthesky@gmail.com>
2022-12-08 11:04:47 -0800

    Update

--- imagen_main.py
+++ imagen_main.py
@@ -206,7 +206,7 @@ def test():
     pb = tqdm(range(100000))
     while True:
         imagen.train_step(jnp.ones((16, 256, 256, 3)), jnp.ones((16,), dtype=jnp.int16), jnp.ones((16, 256, 512)), jnp.ones((16, 256)))
-        imagen.sample(jnp.ones((16, 256, 512)), jnp.ones((16, 256)))
+        # imagen.sample(jnp.ones((16, 256, 512)), jnp.ones((16, 256)))
         pb.update(1)
 #        print("done")
 
