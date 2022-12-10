fd5a582309f01e5e9cab3ad4dcfece7357bf5fce
aakash <aakashinthesky@gmail.com>
2022-12-09 21:21:17 -0800

    Update

--- modeling_imagen.py
+++ modeling_imagen.py
@@ -14,47 +14,48 @@ from config import UnetConfig, ImagenConfig
 
 with_sharding_constraint = nn_partitioning.with_sharding_constraint
 
+
 class EfficentUNet(nn.Module):
     config: UnetConfig
-    
+
     @nn.compact
-    def __call__(self, x:jnp.array, time, texts=None, attention_masks=None, condition_drop_prob=0.0, rng=None):
+    def __call__(self, x: jnp.array, time, texts=None, attention_masks=None, condition_drop_prob=0.0, rng=None):
         x = x.astype(self.config.dtype)
-        time= time.astype(self.config.dtype)
+        time = time.astype(self.config.dtype)
         if exists(texts):
             texts = texts.astype(self.config.dtype)
         if exists(attention_masks):
             attention_masks = attention_masks.astype(self.config.dtype)
-        
+
         x = with_sharding_constraint(x, P("batch", "height", "width", "embed"))
         texts = with_sharding_constraint(texts, P("batch", "length", "embed"))
-        
+
         cond_dim = default(self.config.cond_dim, self.config.dim)
 
-        time_hidden = SinusoidalPositionEmbeddings(config=self.config)(time) # (b, 1, d)
+        time_hidden = SinusoidalPositionEmbeddings(config=self.config)(time)  # (b, 1, d)
         time_hidden = nnp.Dense(features=self.config.time_conditiong_dim, shard_axes={"kernel": ("embed", "mlp")})(time_hidden)
         time_hidden = nn.silu(time_hidden)
 
         t = nnp.Dense(features=self.config.time_conditiong_dim,
-                     dtype=self.config.dtype, shard_axes={"kernel": ("embed", "mlp")})(time_hidden)
-        
+                      dtype=self.config.dtype, shard_axes={"kernel": ("embed", "mlp")})(time_hidden)
+
         t = with_sharding_constraint(t, ("batch", "embed"))
 
         time_tokens = nnp.Dense(self.config.cond_dim * self.config.num_time_tokens, shard_axes={"kernel": ("embed", "mlp")})(t)
         time_tokens = rearrange(time_tokens, 'b (r d) -> b r d', r=self.config.num_time_tokens)
-        
+
         time_tokens = with_sharding_constraint(time_tokens, P("batch", "seq", "embed"))
-        
+
         t, c = TextConditioning(cond_dim=cond_dim, time_cond_dim=self.config.time_conditiong_dim, max_token_length=self.config.max_token_len, cond_drop_prob=condition_drop_prob)(texts, attention_masks, t, time_tokens, rng)
         # TODO: add lowres conditioning
-        
+
         t = with_sharding_constraint(t, ("batch", "embed"))
         c = with_sharding_constraint(c, ("batch", "embed"))
-                
+
         x = CrossEmbedLayer(dim=self.config.dim,
                             kernel_sizes=(3, 7, 15), stride=1)(x)
         x = with_sharding_constraint(x, ("batch", "height", "width", "embed"))
-        
+
         init_conv_residual = x
         # downsample
         hiddens = []
@@ -72,9 +73,9 @@ class EfficentUNet(nn.Module):
         x = ResnetBlock(config=self.config, dim=self.config.dim * self.config.dim_mults[-1])(x, t, c)
         x = EinopsToAndFrom(Attention(config=self.config, dim=self.config.dim * self.config.dim_mults[-1]), 'b h w c', 'b (h w) c')(x)
         x = ResnetBlock(config=self.config, dim=self.config.dim * self.config.dim_mults[-1])(x, t, c)
-        
+
         # Upsample
-        add_skip_connection = lambda x: jnp.concatenate([x, hiddens.pop()], axis=-1)
+        def add_skip_connection(x): return jnp.concatenate([x, hiddens.pop()], axis=-1)
         for dim_mult in reversed(self.config.dim_mults):
             x = add_skip_connection(x)
             x = ResnetBlock(config=self.config, dim=self.config.dim * dim_mult)(x, t, c)
@@ -85,11 +86,11 @@ class EfficentUNet(nn.Module):
                 x = with_sharding_constraint(x, P("batch", "height", "width", "embed"))
             x = TransformerBlock(config=self.config, dim=self.config.dim * dim_mult)(x)
             x = Upsample(config=self.config, dim=self.config.dim * dim_mult)(x)
-        
+
         x = jnp.concatenate([x, init_conv_residual], axis=-1)
-        
+
         x = ResnetBlock(config=self.config, dim=self.config.dim)(x, t, c)
-            
+
         # x = nn.Dense(features=3, dtype=self.dtype)(x)
         x = nn.Conv(features=3, kernel_size=(3, 3), strides=1, dtype=self.config.dtype, padding=1)(x)
-        return x    
+        return x
