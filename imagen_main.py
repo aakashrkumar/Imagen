c090b8429e80fc27359a1d440341257fc51cffe4
aakash <aakashinthesky@gmail.com>
2022-12-12 18:10:35 -0800

    Update

--- imagen_main.py
+++ imagen_main.py
@@ -239,7 +239,7 @@ class Imagen:
                 lowres_cond_image = jnp.ones((batch_size, img_size, img_size, 3)) if unet_config.lowres_conditioning else None # lowres_cond_image
                 lowres_aug_times = jnp.ones(batch_size, dtype=jnp.int16)  if unet_config.lowres_conditioning else None # lowres_aug_times
                 
-                return unet_init(unet, key, image, time_step, text, attention_mask, config.cond_drop_prob, lowres_cond_image, lowres_aug_times)
+                return unet_init(unet, key, image, time_step, text, attention_mask, config.cond_drop_prob, lowres_cond_image, lowres_aug_times, key)
             
                 
             self.random_state, key = jax.random.split(self.random_state)
