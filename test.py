0c2f87c354a67c045400f846d5eac8c018669663
TheRealAakash <aakashinthesky@gmail.com>
2022-12-16 10:04:07 -0800

    Update

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
-from jax.experimental import checkify
 import jax
 import jax.numpy as jnp
 
-def f(x, i):
-  checkify.check(i >= 0, "index needs to be non-negative!")
-  y = x[i]
-  z = jnp.sin(y)
-  return z
-
-jittable_f = checkify.checkify(f)
-
-err, z = jax.jit(jittable_f)(jnp.ones((5,)), -1)
-print(err.get())
+@jax.jit
+def f(x):
+  jax.debug.print("🤯 {x} 🤯", x=x)
+  y = jnp.sin(x)
+  jax.debug.breakpoint()
+  jax.debug.print("🤯 {y} 🤯", y=y)
+  return y
+  
+f(2.)
