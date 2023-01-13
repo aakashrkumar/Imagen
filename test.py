d1bcc9aa058495a088d6581b07dd4c7c0fe0424b
TheRealAakash <aakashinthesky@gmail.com>
2023-01-13 11:11:09 -0800

    Update

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
-
+import time
 while True:
     os.system("gcloud alpha compute tpus tpu-vm create tpu-main --zone europe-west4-a  --accelerator-type='v3-8' --version='tpu-vm-tf-2.9.2' --scopes=https://www.googleapis.com/auth/cloud-platform")
+    time.sleep(1)
