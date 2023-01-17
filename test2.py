417fcfe03fdf70f771436ef9bb769da530c1c1f7
TheRealAakash <aakashinthesky@gmail.com>
2023-01-17 10:29:07 -0800

    Update

--- test2.py
+++ test2.py
@@ -1,18 +1,24 @@
 import matplotlib.pyplot as plt
 import random
 import tqdm
+from concurrent.futures import ProcessPoolExecutor
 
 TRIALS = 1000000
 TOSSES = 1000
 trials = []
-for i in tqdm.tqdm(range(TRIALS)):
-    trial = []
-    for _ in range(TOSSES):
-        trial.append(random.randint(0, 1))
-    trials.append(trial)
 
-for i, trial in enumerate(trials):
-    print(f"Trial {i + 1}: {sum(trial) / TOSSES}")
-# make a histogram of the results
-plt.hist([sum(trial) / TOSSES for trial in trials])
-plt.show()
+def simulate_trial(tosses):
+    trial = []
+    for _ in range(tosses):
+        trial.append(random.randint(0, 1))
+    return trial
+
+if __name__ == "__main__":
+    with ProcessPoolExecutor() as executor:
+        for trial in tqdm.tqdm(executor.map(simulate_trial, [TOSSES for _ in range(TRIALS)]), total=TRIALS):
+            trials.append(trial)
+    for i, trial in enumerate(trials):
+        print(f"Trial {i + 1}: {sum(trial) / TOSSES}")
+    # make a histogram of the results
+    plt.hist([sum(trial) / TOSSES for trial in trials])
+    plt.savefig("histogram.png")
