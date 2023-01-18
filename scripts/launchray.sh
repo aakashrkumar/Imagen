3a823eb8607a4ccc5cb1bf99b2e2f7a01d07ef4a
TheRealAakash <aakashinthesky@gmail.com>
2023-01-18 07:34:22 -0800

    Update

--- scripts/launchray.sh
+++ scripts/launchray.sh
@@ -1,2 +1,2 @@
 ray  stop -f || true
-TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=34359738368 ray start --head --port=6379 --node-ip-address=35.204.25.220 --resources="{\"tpu\": 1, \"host\":999}" --dashboard-host=0.0.0.0
+TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=34359738368 ray start --head --port=6379 --node-ip-address=34.147.22.32 --resources="{\"tpu\": 1, \"host\":999}" --dashboard-host=0.0.0.0
